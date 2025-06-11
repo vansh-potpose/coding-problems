828ba922b69847db4acfb7810c14b7c9c34eb8d1
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-06-11 09:20:17 +0530

    Time: 8 ms (22.29%) | Memory: 16 MB (23.57%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    string shortestPalindrome(string s) {
-        string rev= s;
-        reverse(rev.begin(),rev.end());
-
-        string combined = s+"#"+rev;
-        int n=combined.size();
-        vector<int> lps(n,0);
-
-
-        for(int i=1;i<n;i++){
-            int len=lps[i-1];
-            while(len>0 && combined[i]!=combined[len]){
-                len=lps[len-1];
+    void findLps(string str, vector<int>&lps){
+        int pref=0, suff=1;
+        while(suff < str.length()){
+            if(str[pref] == str[suff]){
+                lps[suff]= pref+1;
+                pref++;
+                suff++;
             }
-            if(combined[i]==combined[len]){
-                len++;
+            else{   //not match
+                if(pref==0)
+                    suff++;
+                else{
+                    pref=lps[pref-1];
+                }
             }
-            lps[i]=len;
         }
+    }
+    string shortestPalindrome(string s) {
+        string rev=s;
+        reverse(rev.begin(), rev.end());
+        string str= s+"$" +rev;
+        vector<int> lps(str.length(),0);
+        findLps(str, lps);
+        int palindromeLen=lps[str.length()-1];
+        rev="";
+        for(int i=s.length()-1;i>=palindromeLen;i--){
+            rev+=s[i];
+        }
+        rev+=s;
+        return rev;
 
-        int palindromeLen = lps.back();
-        string addFront = s.substr(palindromeLen);
-        reverse(addFront.begin(), addFront.end());
-        return addFront + s;
     }
 };
+
