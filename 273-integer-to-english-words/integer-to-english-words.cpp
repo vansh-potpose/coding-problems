65dd5a0411a2db233ad61aea9223adccc50bd028
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-06-13 09:35:57 +0530

    Time: 1 ms (66.96%) | Memory: 9.8 MB (85.19%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
-    vector<string> below_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", 
-                               "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", 
-                               "Seventeen", "Eighteen", "Nineteen"};
-    vector<string> tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    vector<string> thousands = {"", "Thousand", "Million", "Billion"};
+    //globally declare arrays
+    string ones[20] = {"", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen"};
+    string tens[10] = {"", " Ten", " Twenty", " Thirty", " Forty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety"};
+    string thousands[4] = {"", " Thousand", " Million", " Billion"};
+    //helper function
+    string helper(int n) {
+        if (n < 20) 
+            return ones[n];
+        if (n < 100) 
+            return tens[n / 10] + helper(n % 10);
+        if (n < 1000) 
+            return helper(n / 100) + " Hundred" + helper(n % 100);
+        for (int i = 3; i >= 0; i--) {
+            if (n >= pow(1000, i)) {
+                return helper(n / pow(1000, i)) + thousands[i] + helper(n % (int)pow(1000, i));
+            }
+        }
+        return "";
+    }
 
     string numberToWords(int num) {
-        if (num == 0) return "Zero";
-        
-        string result;
-        int i = 0;
-        
-        while (num > 0) {
-            if (num % 1000 != 0) {
-                result = helper(num % 1000) + thousands[i] + (result.empty() ? "" : " " + result);
-            }
-            num /= 1000;
-            i++;
-        }
-
-        return trim(result);
-    }
-    
-    string helper(int num) {
-        if (num == 0)
-            return "";
-        else if (num < 20)
-            return below_20[num] + " ";
-        else if (num < 100)
-            return tens[num / 10] + " " + helper(num % 10);
-        else
-            return below_20[num / 100] + " Hundred " + helper(num % 100);
-    }
-
-    string trim(string s) {
-        // Remove trailing and leading spaces
-        int start = 0;
-        while (start < s.size() && s[start] == ' ') start++;
-        int end = s.size() - 1;
-        while (end >= 0 && s[end] == ' ') end--;
-        return s.substr(start, end - start + 1);
+        // edge case
+        if (num == 0) 
+            return "Zero";
+        return helper(num).substr(1);
     }
 };
