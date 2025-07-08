8b842a2e3689af3212118b6855b417e378fbe970
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-07-08 16:02:54 +0530

    Time: 3 ms (43.31%) | Memory: 10.2 MB (74.10%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-
     string reverseWords(string s) {
         stringstream ss(s);
-        string token;
-        char delimiter=' ';
-        string result="";
-        
-        while (getline(ss, token, delimiter)) {
-            if (!token.empty()) {
-                if(result.empty()){
-                    result=token;
-                }else{
-                result=token + " "+result;
-                }
-            }
+        string str, ans = "";
+        while (ss>>str) {
+            reverse(str.begin(), str.end());
+            ans += str + " ";
         }
-        return result;
+        ans.pop_back();
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
