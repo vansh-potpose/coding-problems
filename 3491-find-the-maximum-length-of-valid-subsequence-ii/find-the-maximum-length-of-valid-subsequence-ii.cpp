942fed4ef2dee68636acbebfe0ae47ca697b4a26
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-07-17 19:43:43 +0530

    Time: 83 ms (98.82%) | Memory: 129.1 MB (43.95%) - LeetSync

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums, int k) {
-        int res = 2;
-        for (int j = 0; j < k; ++j) {
-            vector<int> dp(k, 0);
-            for (int i = 0; i < nums.size(); ++i) {
-                int mod = nums[i] % k;
-                int pos = (j - mod + k) % k;
-                dp[mod] = dp[pos] + 1;
-            }
-
-            for (int val : dp) {
-                res = max(res, val);
-            }
+        int ans = 0 ;
+        for(int i=0 ; i<nums.size() ; i++){
+            nums[i] = nums[i]%k ;
         }
-        return res;
+        int n = nums.size() ;
+        for(int val=0 ; val<k ; val++){
+            vector<int> v(k) ;
+            v[nums[0]] = 1 ;
+            for(int i=1 ; i<n ; i++){
+                int a = nums[i] ;
+                int b = (val-a+k)%k ;
+                v[nums[i]] = 1+v[b] ;
+                ans = max(ans , v[nums[i]]) ;
+            }
+        }   
+        return ans ;
     }
 };
