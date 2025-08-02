c0466a29b46f1c612834fd2aaf0aad80f2bb6d6a
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-08-02 17:27:36 +0530

    Time: 41 ms (66.90%) | Memory: 87.7 MB (92.41%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.cpp
+++ 2689-rearranging-fruits/rearranging-fruits.cpp
@@ -1,45 +1,55 @@
 class Solution {
 public:
-    long long minCost(vector<int>& basket1, vector<int>& basket2) {
-        unordered_map<int, int> freq;
-        map<int, int> total;
-
-        for (int num : basket1) {
-            freq[num]++;
-            total[num]++;
+    long long minCost(vector<int>& B1, vector<int>& B2) {
+        int n = B1.size();
+        unordered_map<int,int> count;
+        int mine = INT_MAX;
+        for(int i=0;i<n;i++){
+            count[B1[i]]++;
+            count[B2[i]]--;
+            
         }
-        for (int num : basket2) {
-            freq[num]--;
-            total[num]++;
+        while(n--){
+            B1.pop_back();
+            B2.pop_back();
         }
-
-        for (auto [num, count] : total) {
-            if (count % 2 != 0) return -1;
-        }
-
-        vector<int> excess;
-
-        for (auto [num, count] : freq) {
-            if (count != 0) {
-                int half = abs(count) / 2;
-                for (int i = 0; i < half; ++i)
-                    excess.push_back(num);
+        for(auto it: count){
+            if((it.second)%2!=0){
+                return -1;
             }
+            
+            if(it.second>0){
+                for(int i=0;i<it.second/2;i++){
+                    B1.push_back(it.first);
+                }
+
+            }
+            else{
+                for(int i=0;i<-it.second/2;i++){
+                    B2.push_back(it.first);
+                }
+            }
+            
+            mine=min(mine,it.first);
         }
-
-        sort(excess.begin(), excess.end());
-        int n = excess.size();
-
-        int min_elem = min(*min_element(basket1.begin(), basket1.end()),
-                       *min_element(basket2.begin(), basket2.end()));
-
-
-        long long cost = 0;
-        for (int i = 0; i < n / 2; ++i) {
-            cost += min((long long)excess[i], 2LL * min_elem);
+        sort(B1.begin(),B1.end());
+        sort(B2.begin(),B2.end());
+        
+        int l1=0, l2=0, r1=B1.size()-1,r2=B2.size()-1;
+        long long ans = 0;
+        while(l1<=r1 && l2<=r2){
+            if(min(B1[l1],B2[r2])<2*mine){
+                ans+=min(B1[l1],B2[r2]);
+                l1++;
+                r2--;
+            }
+            else{
+                ans+=2*mine;
+                r1--;
+                r2--;
+            }
+            
         }
-
-        return cost;
-
+        return ans;
     }
 };
