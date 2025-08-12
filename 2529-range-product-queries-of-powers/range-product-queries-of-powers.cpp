cb21370852888455497962bd7b5dfd6829ce9f37
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-08-12 09:16:16 +0530

    Time: 4 ms (95.19%) | Memory: 145.1 MB (79.68%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,51 +1,20 @@
 class Solution {
 public:
-    const int MOD = 1000000007;
-    
-    // Modular exponentiation
-    long long modpow(long long base, long long exp) {
-        long long result = 1;
-        while (exp > 0) {
-            if (exp & 1) result = (result * base) % MOD;
-            base = (base * base) % MOD;
-            exp >>= 1;
+    vector<int> productQueries(int n, const vector<vector<int>>& queries) {
+        constexpr int MOD = 1000000007;
+        vector prefix {0};
+        while (n) {
+            const int j = __builtin_ctz(n);
+            prefix.push_back(prefix.back() + j);
+            n -= 1 << j;
         }
-        return result;
-    }
-    
-    // Convert n to list of powers of two
-    vector<int> getNum(int n) {
-        vector<int> num;
-        for (int i = 0; i < 31; i++) {
-            if (n & (1 << i)) {
-                num.push_back(1 << i);
-            }
-        }
-        return num;
-    }
-    
-    vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        vector<int> num = getNum(n);
-        
-        // Prefix products
-        vector<long long> prefix_prod(num.size());
-        prefix_prod[0] = num[0] % MOD;
-        for (int i = 1; i < num.size(); i++) {
-            prefix_prod[i] = (prefix_prod[i-1] * num[i]) % MOD;
-        }
-        
-        vector<int> ans;
-        for (auto &q : queries) {
-            int l = q[0], r = q[1];
-            if (l == 0) {
-                ans.push_back(prefix_prod[r]);
-            } else {
-                long long numerator = prefix_prod[r];
-                long long denominator = prefix_prod[l-1];
-                long long inverse = modpow(denominator, MOD - 2);
-                ans.push_back((numerator * inverse) % MOD);
-            }
-        }
-        return ans;
+        n = prefix.back();
+        vector ans {1}; ans.reserve(n);
+        for (int i = 1; i <= n; i++)
+            ans.push_back((ans.back() << 1) % MOD);
+        vector<int> ret; ret.reserve(queries.size());
+        for (const auto& q : queries)
+            ret.push_back(ans[prefix[q[1] + 1] - prefix[q[0]]]);
+        return ret;
     }
 };
