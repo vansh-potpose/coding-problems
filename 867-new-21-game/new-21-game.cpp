c47086e30c06cf186f7d9d43e84f06d6f57c7c23
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-08-17 12:25:12 +0530

    Time: 4 ms (26.82%) | Memory: 12.7 MB (33.18%) - LeetSync

--- 867-new-21-game/new-21-game.cpp
+++ 867-new-21-game/new-21-game.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     double new21Game(int n, int k, int maxPts) {
-        if (k == 0 || n >= k - 1 + maxPts) return 1.0;
+        if (k == 0) return 1.0;
+        if (n >= k + maxPts - 1) return 1.0;
 
-        vector<double> dp(maxPts, 0.0);
+        vector<double> dp(n + 1, 0.0);
         dp[0] = 1.0;
+        double windowSum = 1.0;
+        double result = 0.0;
 
-        double windowSum = 1.0, result = 0.0;
-
-        for (int i = 1; i <= n; i++) {
-            double prob = windowSum / maxPts;
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = windowSum / maxPts;
 
             if (i < k) {
-                windowSum += prob;
+                windowSum += dp[i];
             } else {
-                result += prob;
+                result += dp[i];
             }
-
-            if (i >= maxPts) {
-                windowSum -= dp[i % maxPts];
+            if (i - maxPts >= 0) {
+                windowSum -= dp[i - maxPts];
             }
-
-            dp[i % maxPts] = prob;
         }
 
         return result;
