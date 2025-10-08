0800f9dd9245f9042053cd8928e0cb063eeb755d
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-10-08 19:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (94.11%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,26 +1,36 @@
-#include <unordered_map>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
+    int sym(char c) {
+        switch (c) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return -1;
+        }
+    }
+
     int romanToInt(string s) {
-        unordered_map<char, int> roman = {
-            {'I',1}, {'V',5}, {'X',10}, {'L',50},
-            {'C',100}, {'D',500}, {'M',1000}
-        };
-        
-        int result = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            int curr = roman[s[i]];
-            int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
-            
-            if (curr < next) {
-                result -= curr;
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i == s.length() - 1 || sym(s[i]) >= sym(s[i + 1])) {
+                ans += sym(s[i]);
             } else {
-                result += curr;
+                ans -= sym(s[i]);
             }
         }
-        return result;
+        return ans;
     }
 };
