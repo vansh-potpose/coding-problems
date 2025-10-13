b63acebf6f052053c1612f714fff64f251aadad2
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-10-13 19:05:54 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (70.62%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,25 +1,10 @@
-
-
 class Solution {
 public:
-    std::vector<int> findErrorNums(std::vector<int>& nums) {
-        int dup = -1, missing = -1;
-        
-        for (int i = 1; i <= nums.size(); i++) {
-            int count = 0;
-            for (int j = 0; j < nums.size(); j++) {
-                if (nums[j] == i) {
-                    count++;
-                }
-            }
-            if (count == 2) {
-                dup = i;
-            } else if (count == 0) {
-                missing = i;
-            }
-        }
-        
-        return {dup, missing};
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        long a = n*(n+1)>>1;
+        long b = a*((n<<1)+1)/3;
+        for (int i : nums) {a -= i; b -= i*i;}
+        return {static_cast<int>((b-a*a)/a/2), static_cast<int>((b+a*a)/a/2)};
     }
 };
-
