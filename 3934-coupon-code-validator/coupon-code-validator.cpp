8f87d62b3f27ba38f8e2c071345076a350d4b9be
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2025-12-13 09:44:26 +0530

    Time: 16 ms (11.59%) | Memory: 69.3 MB (56.22%) - LeetSync

--- 3934-coupon-code-validator/coupon-code-validator.cpp
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -1,34 +1,33 @@
 class Solution {
-    bool isAlphanumericOrUnderscore(const std::string& str) {
-    if(!str.size()) return false;
-    return all_of(str.begin(), str.end(), [](unsigned char c) {
-        return isalnum(c) || c == '_';
-    });
-}
 public:
     vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
-        vector<pair<string,string>> temp;
-        unordered_set<string> b={"electronics", "grocery", "pharmacy", "restaurant"};
-
-        for(int i=0;i<code.size();i++){
-            if(isActive[i] && b.count(businessLine[i]) && isAlphanumericOrUnderscore(code[i])){
-                temp.push_back({code[i],businessLine[i]});
+        unordered_map<string, vector<string>> m;
+        int n = code.size();
+        for (int i = 0; i < n; i++)
+        {
+            bool flag = isActive[i] && (businessLine[i] == "electronics" || 
+            businessLine[i] == "grocery" || businessLine[i] == "pharmacy" || 
+            businessLine[i] == "restaurant") && code[i].size() > 0;
+            if (flag)
+            {
+                for (auto& c : code[i])
+                    if (!(isalpha(c) || isdigit(c) || c == '_'))
+                    {
+                        flag = false;
+                        break;
+                    }
             }
+            if (flag)
+                m[businessLine[i]].push_back(code[i]);
         }
-
-        sort(temp.begin(),temp.end(),[](const pair<string,string> & a,const pair<string,string> & b){
-            if(a.second==b.second){
-                return a.first<b.first;
-            }else{
-                return a.second<b.second;
-            }
-        });
-
-
-        vector<string> result;
-        for(int i=0;i<temp.size();i++){
-            result.push_back(temp[i].first);
+        vector<string> cats = {"electronics", "grocery", "pharmacy", "restaurant"}; 
+        vector<string> ans;
+        for (auto& cat : cats)
+        {
+            sort(m[cat].begin(), m[cat].end());
+            for (auto& coupon : m[cat])
+                ans.push_back(coupon);
         }
-        return result;
+        return ans;
     }
 };
