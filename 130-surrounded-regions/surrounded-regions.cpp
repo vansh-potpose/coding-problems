20d23c93d48548a6c31e0a2bd9b389b0387f87e5
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2026-01-11 14:18:08 +0530

    Time: 2 ms (28.16%) | Memory: 14.6 MB (36.24%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,47 +1,38 @@
 class Solution {
-private:
-    int M, N;
+public:
+    void dfs(int r, int c, vector<vector<char>>&board, vector<vector<int>>&vis, int delrow[], int delcol[]){
+        vis[r][c] = 1;
+        for(int i=0;i<4;i++){
+            int nr = r + delrow[i];
+            int nc = c + delcol[i];
 
-    void dfs(int i, int j, vector<vector<char>>& board, vector<vector<bool>>& vis) {
-        // Base case: out of bounds, already visited, or 'X'
-        if (i < 0 || i >= M || j < 0 || j >= N || vis[i][j] || board[i][j] == 'X') {
-            return;
+            if(nr>=0 && nr<board.size() && nc>=0 && nc<board[0].size() && board[nr][nc]=='O' && !vis[nr][nc]){
+                dfs(nr, nc, board, vis, delrow, delcol);
+            }
         }
-        
-        vis[i][j] = true;
-
-        // DFS on neighbors
-        dfs(i + 1, j, board, vis);
-        dfs(i - 1, j, board, vis);
-        dfs(i, j + 1, board, vis);
-        dfs(i, j - 1, board, vis);
     }
 
-public:
     void solve(vector<vector<char>>& board) {
-        M = board.size();
-        N = board[0].size();
-        vector<vector<bool>> vis(M, vector<bool>(N, false));
+        int n = board.size(), m = board[0].size();
 
-        // 1 & 2. DFS from boundary 'O's to mark unflippable regions
-        // Left and Right borders
-        for (int i = 0; i < M; ++i) {
-            if (board[i][0] == 'O' && !vis[i][0]) dfs(i, 0, board, vis);
-            if (board[i][N - 1] == 'O' && !vis[i][N - 1]) dfs(i, N - 1, board, vis);
+        vector<vector<int>>vis(n, vector<int>(m,0));
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, -1, 0, 1};
+        for(int i=0;i<m;i++){
+            if(!vis[0][i] && board[0][i] == 'O') dfs(0,i,board,vis, delrow, delcol);
         }
-        // Top and Bottom borders
-        for (int j = 0; j < N; ++j) {
-            if (board[0][j] == 'O' && !vis[0][j]) dfs(0, j, board, vis);
-            if (board[M - 1][j] == 'O' && !vis[M - 1][j]) dfs(M - 1, j, board, vis);
+        for(int i=0;i<m;i++){
+            if(!vis[n-1][i] && board[n-1][i] == 'O') dfs(n-1,i,board,vis, delrow, delcol);
         }
-
-        // 3. Flip surrounded 'O's
-        for (int i = 0; i < M; ++i) {
-            for (int j = 0; j < N; ++j) {
-                // If 'O' is not visited, it means it's surrounded
-                if (board[i][j] == 'O' && !vis[i][j]) {
-                    board[i][j] = 'X';
-                }
+        for(int i=0;i<n;i++){
+            if(!vis[i][0] && board[i][0] == 'O') dfs(i,0,board,vis, delrow, delcol);
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i][m-1] && board[i][m-1] == 'O') dfs(i,m-1,board,vis, delrow, delcol);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && board[i][j] == 'O') board[i][j] = 'X';
             }
         }
     }
