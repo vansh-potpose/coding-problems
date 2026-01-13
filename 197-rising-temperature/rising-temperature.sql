192b27e0a4a60d229ad7b7534e1f5a2070ec4c45
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2026-01-13 11:03:16 +0530

    Time: 424 ms (63.79%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,7 +1,6 @@
-SELECT today.id
-FROM Weather yesterday 
-CROSS JOIN Weather today
-
-WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
-    AND today.temperature > yesterday.temperature
-;
+# Write your MySQL query statement below
+select w1.id
+from Weather w1
+join Weather w2
+on  w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+where w1.temperature >w2.temperature
