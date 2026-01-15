38f87e4a62e6e1c48dfd8ca963a99d9987b6e8eb
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2026-01-15 10:25:56 +0530

    Time: 1 ms (22.61%) | Memory: 32.7 MB (7.83%) - LeetSync

--- 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    static inline int findLen(vector<int>& bars){
-        sort(bars.begin(), bars.end());
-        const int bz=bars.size();
-        int Len=1, maxLen=1;
-        for(int i=0; i<bz-1; i++){
-            Len=1+(-(bars[i]+1==bars[i+1]) & Len);
-            maxLen=max(Len, maxLen);
+    int findgap( vector<int> bars){
+        sort(bars.begin(),bars.end());
+
+        int count = 2, res = 2;
+        for(int i=1;i<bars.size();i++){
+            // consecutive bars
+            if(bars[i-1] + 1 == bars[i]){
+                count++;
+            }
+            else{
+                count = 2;
+            }
+            res = max(count,res);
         }
-        return maxLen;
+        return res;
     }
-    static int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
-        int Len=1+min(findLen(hBars), findLen(vBars));
-        return Len*Len;
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        // find horizontal gap and vertical gap
+        int gap = min(findgap(hBars),findgap(vBars));
+        // for square take min of horizonal and vertical
+        return gap * gap;
     }
 };
