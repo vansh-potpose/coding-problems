f29821d30c30139ba0e6921b6999426934212be5
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2026-01-19 10:06:24 +0530

    Time: 4 ms (31.76%) | Memory: 10.7 MB (48.56%) - LeetSync

--- 482-license-key-formatting/license-key-formatting.cpp
+++ 482-license-key-formatting/license-key-formatting.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
-        
-        // Eliminate all dashes
-        s = std::regex_replace(s, std::regex("\-"), "");
-        
-        int head = s.length() % k;
-        
-        vector<string> grouping;
-        
-        // Special handle for first group
-        if( head ){
-            grouping.emplace_back( s.substr(0, head) );
+        int cur=k;
+        string ans;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='-') continue;
+            if(cur==0){
+                cur=k;
+                ans.push_back('-');
+            }
+            ans.push_back(toupper(s[i]));
+            cur--;
         }
-        
-        // General cases
-        for( int i = head ; i < s.length() ; i += k ){
-             grouping.emplace_back( s.substr(i, k) );
-        }
-        
-        // Link each group togetger and separated by dash '-'
-        s = join(grouping, "-");
-        
-        // to uppercase
-        transform(s.begin(), s.end(), s.begin(), [](unsigned char c){ return std::toupper(c); } );
-        return s;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
-    
-private:
-    string join(const std::vector<string> &lst, const string &delim)
-    {
-        std::string ret;
-        for(const auto &s : lst) {
-            if(!ret.empty())
-                ret += delim;
-            ret += s;
-        }
-        return ret;
-    }
-    
 };
