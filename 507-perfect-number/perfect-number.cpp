ef49d99b5830c1b8b1192e2a497a3c3ced89dadd
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2026-01-26 09:34:54 +0530

    Time: 1040 ms (44.45%) | Memory: 7.7 MB (87.79%) - LeetSync

---
+++ 507-perfect-number/perfect-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<=num/2;i++){
+            if(num%i==0){
+                sum+=i;
+                cout<<i<<" ";
+            }
+        }
+        return sum==num;
+    }
+};
