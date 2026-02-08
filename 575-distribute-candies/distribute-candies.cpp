a539960e0ff8be560eb54d1568cde4978a6cc7ab
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2026-02-08 12:30:42 +0530

    Time: 0 ms (100.00%) | Memory: 85.8 MB (84.58%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        unordered_set<int> candytype;
-        for(int i=0;i<candyType.size();i++){
-            if(candytype.find(candyType[i])==candytype.end()){
-                candytype.insert(candyType[i]);
+        bool typeSeen[200001] = {false};
+        int uniqueTypeCount = 0;
+        int maxCandies = candyType.size() / 2;
+        for (int type : candyType) {
+            if (!typeSeen[type + 100000]) {
+                typeSeen[type + 100000] = true;
+                uniqueTypeCount++;
+                if (uniqueTypeCount >= maxCandies) {
+                    return maxCandies;
+                }
             }
         }
-        return min(candyType.size()/2,candytype.size());
+        return uniqueTypeCount;
     }
 };
