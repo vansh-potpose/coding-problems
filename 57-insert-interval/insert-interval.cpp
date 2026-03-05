bed3a0840661e85a81b8b335167f0e39ffb70442
vansh potpose <156517528+vansh-potpose@users.noreply.github.com>
2026-03-05 14:11:20 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (47.36%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> result;
+
+        for (auto& interval : intervals) {
+            if (interval[1] < newInterval[0]) {
+                result.push_back(interval);
+            } else if (interval[0] > newInterval[1]) {
+                result.push_back(newInterval);
+                newInterval = interval;
+            } else {
+                newInterval[0] = min(interval[0], newInterval[0]);
+                newInterval[1] = max(interval[1], newInterval[1]);
+            }
+        }
+
+        result.push_back(newInterval);
+        return result;
+    }
+};
